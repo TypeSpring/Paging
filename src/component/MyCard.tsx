ec31cfe55b8a574ccb237b10a306c8ee77555fcf
Hyevvy <hyeg0@naver.com>
2022-03-27 03:22:58 +0900

    feat: 페이지네이션 구현

--- src/component/MyCard.tsx
+++ src/component/MyCard.tsx
@@ -3,23 +3,31 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { CardProps } from "../page/InfScroll";
 
-const MyCard: FC = () => {
+export default function MyCard({ CardData }: { CardData: CardProps }) {
+  const { thumbnail, author, contents, title } = CardData;
   return (
     <>
-      <Card>
-        <CardMedia component="img" alt="thumbnail" height="140" image="" />
+      <Card sx={{ width: "200px" }} className="MyCard">
+        <CardMedia
+          component="img"
+          alt="thumbnail"
+          height="140"
+          image={thumbnail}
+        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            title
+            title : {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            body
+            author : {author}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            content : {contents}
           </Typography>
         </CardContent>
       </Card>
-      ;
     </>
   );
-};
-export default MyCard;
+}
--- src/page/MyPagination.tsx
+++ src/page/MyPagination.tsx
@@ -1,13 +1,52 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import Pagination from "@mui/material/Pagination";
+import { CardProps } from "./InfScroll";
+import { request } from "../config/axios";
+import MyCard from "../component/MyCard";
 
 const MyPagination: FC = () => {
-  return (
-    <>
-      <div>페이지네이션</div>
+  const [list, setList] = useState<CardProps[]>([]);
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [totalPage, setTotalPage] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-      <Pagination count={10} color="secondary" />
-    </>
+  //클라이언트측에서 서버에게 한 페이지당 몇개의 게시글을 달라고 요청할 지 지정
+  //이 프로젝트에서는 지정 안 하고, 서버에서 default로 주는 값 사용함
+  // const [postsPerPage, setPostsPerPage] = useState<number>(20);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      await request.get(`?page=${currentPage - 1}`).then((res) => {
+        setList(res.data.content);
+        setLoading(false);
+
+        //데이터를 받아올 때 총 페이지를 받아온다.
+        setTotalPage(res.data.totalPages);
+      });
+    }
+    fetchData();
+  }, [currentPage]);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    console.log(currentPage);
+    setCurrentPage(value);
+  };
+  return (
+    <div className="MyPagination">
+      <div>
+        {list && list.map((item, idx) => <MyCard key={idx} CardData={item} />)}
+      </div>
+      <Pagination
+        page={currentPage}
+        onChange={handlePageChange}
+        count={totalPage}
+        color="secondary"
+      />
+    </div>
   );
 };
 export default MyPagination;
