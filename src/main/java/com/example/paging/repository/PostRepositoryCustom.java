35df278d3056f753a0252b05cf648dd851940202
DolphaGo <adamdoha@naver.com>
2022-03-27 03:24:48 +0900

    feat: Paging

--- src/main/java/com/example/paging/repository/PostRepositoryCustom.java
+++ src/main/java/com/example/paging/repository/PostRepositoryCustom.java
@@ -1,11 +1,10 @@
 package com.example.paging.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.example.paging.dto.PostDto;
 
 public interface PostRepositoryCustom {
-    List<PostDto> search(Pageable pageable);
+    Page<PostDto> search(Pageable pageable);
 }
